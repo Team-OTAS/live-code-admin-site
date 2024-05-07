66a4aa587825fb1c2b8fb52491459ce04804789e
aaog2 <aungaungoogame2@gmail.com>
2024-05-07 12:24:53 +0630

    feat: add update receipt feature

--- src/Components/Accsetting/ShopReciept.jsx
+++ src/Components/Accsetting/ShopReciept.jsx
@@ -5,12 +5,80 @@ import ReceiptLongOutlinedIcon from "@mui/icons-material/ReceiptLongOutlined";
 import axios from "./../../api/axios";
 import { getShopData } from "./../../redux/features/shopDataSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import Swal from "sweetalert2";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 function ShopReciept() {
   const dispatch = useDispatch();
-  const id = localStorage.getItem("shopId");
+  const [edit, setEdit] = useState(true);
+  const [file, setFile] = useState(null);
+  const [local, setlocal] = useState(false);
   const { loading, error, shopData } = useSelector((state) => state.ShopData);
-  // console.log(id);
+  const [logo, setLogo] = useState(shopData.data.logo);
+  const [footer, setFooter] = useState(shopData.data.receipt_footer);
+  const [header, setHeader] = useState(shopData.data.receipt_header);
+  const id = localStorage.getItem("shopId");
+
+  function hundleFileChange(e) {
+    setFile(e.target.files[0]);
+    setlocal(true);
+  }
+
+  const saveReceipt = async () => {
+    setEdit(false);
+    const data = {
+      logo: local ? file : null,
+      receipt_header: header,
+      receipt_footer: footer,
+    };
+    try {
+      const res = await axios.post(
+        `api/shops/${id}/shop-receipt?_method=PATCH`,
+        data,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(res);
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Updated",
+      });
+      dispatch(getShopData(id));
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: error.response.data.message,
+      });
+    }
+  };
+
+  const discardEdit = async () => {
+    setlocal(false);
+    setEdit(true);
+    setFooter(shopData.data.receipt_footer);
+    setHeader(shopData.data.receipt_header);
+  };
+
   useEffect(() => {
     dispatch(getShopData(id));
   }, []);
@@ -34,18 +102,30 @@ function ShopReciept() {
               }}
             >
               <p className="page-header">Shop Receipt</p>
-              <button className="edit-btn">
-                Edit Shop Receipt{" "}
-                <EditRoundedIcon
-                  sx={{
-                    background: "#4D3F3F",
-                    borderRadius: "50%",
-                    color: "white",
-                    padding: "5px",
-                    marginLeft: "20px",
-                  }}
-                />
-              </button>
+              {edit && (
+                <button className="edit-btn" onClick={() => setEdit(false)}>
+                  Edit Shop Receipt{" "}
+                  <EditRoundedIcon
+                    sx={{
+                      background: "#4D3F3F",
+                      borderRadius: "50%",
+                      color: "white",
+                      padding: "5px",
+                      marginLeft: "20px",
+                    }}
+                  />
+                </button>
+              )}
+              {!edit && (
+                <div className="btn-container">
+                  <button className="discard" onClick={discardEdit}>
+                    Discard
+                  </button>
+                  <button className="save" onClick={saveReceipt}>
+                    Save Edit
+                  </button>
+                </div>
+              )}
             </div>
           </div>
           <Grid container spacing={2} sx={{ paddingRight: "25px" }}>
@@ -53,21 +133,42 @@ function ShopReciept() {
               <Box className="shopLogo">
                 <p className="title-logo">Shop Logo</p>
                 <div>
-                  <div className="logo-name">
-                    <p style={{ color: "white" }}>Backplates Nonntal 02 0138</p>
-                  </div>
                   <div
                     style={{
                       display: "flex",
                       justifyContent: "center",
+                      flexDirection: "column",
+                      padding: "0 20px",
                     }}
                   >
-                    <div className="logo-img">
-                      <img
-                        src={`https://api.livecodemm.com${shopData.data.logo}`}
-                        alt="logo"
-                      />
-                    </div>
+                    {local ? (
+                      <div className="logo-img">
+                        <img src={URL.createObjectURL(file)} alt="logo" />
+                      </div>
+                    ) : (
+                      <div className="logo-img">
+                        <img
+                          src={`https://api.livecodemm.com${logo}`}
+                          alt="logo"
+                        />
+                      </div>
+                    )}
+                    {!edit && (
+                      <Button
+                        component="label"
+                        role={undefined}
+                        variant="contained"
+                        tabIndex={-1}
+                        startIcon={<CloudUploadIcon />}
+                        sx={{ margin: "10px 20px" }}
+                      >
+                        Upload file
+                        <VisuallyHiddenInput
+                          type="file"
+                          onChange={hundleFileChange}
+                        />
+                      </Button>
+                    )}
                   </div>
                 </div>
               </Box>
@@ -83,8 +184,9 @@ function ShopReciept() {
                     type="text"
                     className="receipt-body"
                     rows={6}
-                    value={shopData.data.receipt_header}
-                    readOnly={true}
+                    value={header}
+                    onChange={(e) => setHeader(e.target.value)}
+                    readOnly={edit}
                   ></textarea>
                 </div>
               </Box>
@@ -100,8 +202,9 @@ function ShopReciept() {
                     type="text"
                     className="receipt-body"
                     rows={6}
-                    value={shopData.data.receipt_footer}
-                    readOnly={true}
+                    value={footer}
+                    onChange={(e) => setFooter(e.target.value)}
+                    readOnly={edit}
                   ></textarea>
                 </div>
               </Box>
