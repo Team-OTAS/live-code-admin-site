c97e22f59aa06b4a142811ebf63f79dd34d93c5a
aaog2 <aungaungoogame2@gmail.com>
2024-05-25 22:01:19 +0630

    feat: unistall react facebbook

--- src/Components/liveSale/Checkbox.jsx
+++ src/Components/liveSale/Checkbox.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import { FacebookProvider, Page } from "react-facebook";
+// import { FacebookProvider, Page } from "react-facebook";
 
 export default class LiveCheckbox extends Component {
   render() {
-    return (
-      <FacebookProvider appId="872263577698057">
-        <Page href="https://www.facebook.com" tabs="timeline" />
-      </FacebookProvider>
-    );
+    return <div>Testing Chat</div>;
   }
 }
