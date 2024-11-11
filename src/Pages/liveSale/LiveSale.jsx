16fe31054cf78ed52e7ca318e908c03b1c7a4c81
aaog2 <aungaungoogame2@gmail.com>
2024-11-11 22:30:30 +0630

    feat: echo off

--- src/Pages/liveSale/LiveSale.jsx
+++ src/Pages/liveSale/LiveSale.jsx
@@ -1,47 +1,48 @@
 import { Box, Grid } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import LiveDataTable from "../../Components/liveSale/LiveDataTable";
-import useEcho from "../../hook/echo";
-import LiveNotifincation from "./../../Components/liveSale/LiveNotifincation";
+// import useEcho from "../../hook/echo";
+// import LiveNotifincation from "./../../Components/liveSale/LiveNotifincation";
 
 function LiveSale() {
-  const [liveData, setLiveData] = useState([]);
+  // const [liveData, setLiveData] = useState([]);
   // const [orderSuccessMessage, setOrderSuccessMessage] = useState([]);
-  const echo = useEcho();
-  const shopId = localStorage.getItem("shopId");
+  // const echo = useEcho();
+  // const shopId = localStorage.getItem("shopId");
   // console.log("live page", liveData);
 
-  useEffect(() => {
-    // console.log("echo", echo);
+  // useEffect(() => {
+  //   // console.log("echo", echo);
 
-    if (echo) {
-      echo.private(`shop.${shopId}.products`).listen("ProductUpdated", (e) => {
-        // console.log("product Update Event", e);
-        setLiveData(e);
-      });
+  //   if (echo) {
+  //     echo.private(`shop.${shopId}.products`).listen("ProductUpdated", (e) => {
+  //       // console.log("product Update Event", e);
+  //       setLiveData(e);
+  //     });
 
-      // echo
-      //   .private(`shop.${shopId}.order-success-message`)
-      //   .listen("OrderSuccessMessage", (e) => {
-      //     console.log("Order Success Message Event", e);
-      //     setOrderSuccessMessage(e);
-      //   });
-    }
-  }, [echo]);
+  //     // echo
+  //     //   .private(`shop.${shopId}.order-success-message`)
+  //     //   .listen("OrderSuccessMessage", (e) => {
+  //     //     console.log("Order Success Message Event", e);
+  //     //     setOrderSuccessMessage(e);
+  //     //   });
+  //   }
+  // }, [echo]);
 
   return (
     <div className="dashboardContent">
       <Box>
         <Grid container>
           <Grid item xs={12} md={8}>
-            <LiveDataTable liveData={liveData} />
+            {/* <LiveDataTable liveData={liveData} /> */}
+            <LiveDataTable />
           </Grid>
           <Grid item xs={12} md={4}>
             {/* <NotificationArea /> */}
-            <LiveNotifincation
+            {/* <LiveNotifincation
               // orderSuccessMessage={orderSuccessMessage}
               liveData={liveData}
-            />
+            /> */}
           </Grid>
         </Grid>
       </Box>
